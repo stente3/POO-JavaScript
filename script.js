34cb18e008c3ccec198cb7c5ca24aec5986feeb0
stente3 <stentem0103@gmail.com>
2021-08-28 12:55:12 -0500

    added form functionality

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-class text {
+class TextBlock {
 	constructor(nombre, price) {
 		this.nombre = nombre;
 		this.price = price;
 	}
 }
-const button = document
-	.querySelector(".button")
-	.addEventListener("click", (e) => {
-		const nombre = document.querySelector("#nombre").value;
-		const número = document.querySelector("#número").value;
-		let book = new text(nombre, número);
-		console.log(book);
-		e.preventDefault();
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	"use strict";
+
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.querySelectorAll(".needs-validation");
+
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms).forEach(function (form) {
+		form.addEventListener(
+			"submit",
+			function (event) {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				} else {
+					event.preventDefault();
+					const productName = document.querySelector("#product").value;
+					const productPrice = document.querySelector("#price").value;
+					console.log(productName);
+					console.log(productPrice);
+				}
+
+				form.classList.add("was-validated");
+			},
+			false
+		);
 	});
+})();
