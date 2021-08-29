f0c17a0fe5dba362ec26b8cd6a0b82b197122c03
stente3 <stentem0103@gmail.com>
2021-08-29 10:50:36 -0500

    Content design was added and also its configuration with JavaScript

--- script.js
+++ script.js
@@ -23,8 +23,19 @@ class TextBlock {
 					event.preventDefault();
 					const productName = document.querySelector("#product").value;
 					const productPrice = document.querySelector("#price").value;
-					console.log(productName);
-					console.log(productPrice);
+					const productDescription =
+						document.querySelector("#description").value;
+					const tableOfContent = document.querySelector(".table-of-content");
+					let content = document.createElement("div");
+					content.innerHTML = `<div class="container-all-content text-center py-4 border mb-2 w-100">
+					<div class="container-button col-md-10 m-md-auto d-flex my-md-0 justify-content-around">
+						<p class="product-name"><strong>Product Name = </strong> ${productName}</p>
+						<p class="product-price"><strong>Product Price = </strong> ${productPrice}</p>
+						<p class="product-description mw-50">${productDescription}</p>
+					</div>
+					<input type="button" value="Eliminar" class="bg-danger btn">
+				</div>`;
+					tableOfContent.appendChild(content);
 				}
 
 				form.classList.add("was-validated");
