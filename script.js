c14642c03d58745d063c91957c119ab5c564fdbd
stente3 <stentem0103@gmail.com>
2021-09-03 19:43:50 -0500

    the project is finished

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 class TextBlock {
-	constructor(nombre, price) {
-		this.nombre = nombre;
+	constructor(name, price, description) {
+		this.name = name;
 		this.price = price;
+		this.description = description;
 	}
 }
-// Reset form validation
-function wasValidate() {
-	document.querySelector(".needs-validation").classList.remove("was-validated");
-	let reset = document.querySelectorAll(".needs-validation .form-control");
-	reset.forEach((e) => {
-		e.value = "";
-	});
+document.querySelector("#product").focus();
+// this class contains all the content that interacts with the client
+class UI {
+	resetForm() {
+		let form = document.querySelector(".needs-validation");
+		form.classList.remove("was-validated");
+		form.reset();
+		document.querySelector("#product").focus();
+	}
+	addContent() {
+		const name = document.querySelector("#product").value;
+		const price = document.querySelector("#price").value;
+		const description = document.querySelector("#description").value;
+		let newContent = new TextBlock(name, price, description);
+		const tableOfContent = document.querySelector(".table-of-content");
+		let content = document.createElement("div");
+		content.classList.add("mw-100");
+		content.innerHTML = `<div class="container-all-content text-center p-4 mb-2 w-100 border">
+			<div class="container-button col-md-10 m-md-auto my-md-0 row">
+					<p class="product-name col-6">
+						<strong>Product Name = </strong> ${newContent.name}
+					</p>
+					<p class="product-price col-6">
+						<strong>Product Price = </strong> $ ${newContent.price}
+					</p>
+					<p class="product-description mw-50 col-12 text-start">
+						${newContent.description}
+					</p>
+			</div>
+				<input type="button" value="Eliminar" class="btn-remove bg-danger btn" />
+		</div>`;
+		tableOfContent.appendChild(content);
+	}
+	removeContent() {
+		const button = document.querySelectorAll(".btn-remove");
+		button.forEach((e) => {
+			e.addEventListener("click", (e) => {
+				e.target.parentElement.parentElement.remove();
+			});
+		});
+	}
 }
-// Add table of content with new
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
 	"use strict";
@@ -31,38 +65,12 @@ function wasValidate() {
 					form.classList.add("was-validated");
 				} else {
 					event.preventDefault();
-					const productName = document.querySelector("#product").value;
-					const productPrice = document.querySelector("#price").value;
-					const productDescription =
-						document.querySelector("#description").value;
-					wasValidate();
-					const tableOfContent = document.querySelector(".table-of-content");
-					let content = document.createElement("div");
-					content.classList.add("mw-100");
-					content.innerHTML = `<div class="container-all-content text-center p-4 mb-2 w-100 border">
-					<div class="container-button col-md-10 m-md-auto my-md-0 row">
-						<p class="product-name col-6">
-							<strong>Product Name = </strong> ${productName}
-						</p>
-						<p class="product-price col-6">
-							<strong>Product Price = </strong> $ ${productPrice}
-						</p>
-						<p class="product-description mw-50 col-12 text-start">
-							${productDescription}
-						</p>
-					</div>
-				</div>`;
-					let btnRemove = document.createElement("input");
-					btnRemove.type = "button";
-					btnRemove.value = "Eliminar";
-					btnRemove.classList.add("bg-danger", "btn");
-					btnRemove.onclick = (e) => {
-						btnRemove.parentElement.parentElement.remove();
-					};
-					//<input type="button" value="Eliminar" class="bg-danger btn" />
-					tableOfContent.appendChild(content);
-					content.firstChild.appendChild(btnRemove);
-					wasValidate();
+					let reset = new UI();
+					reset.addContent();
+					let userInter = new UI();
+					userInter.resetForm();
+					let remove = new UI();
+					userInter.removeContent();
 				}
 			},
 			false
